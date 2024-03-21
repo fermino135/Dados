7b33183c87cba30bdef9ba666bc95f0c3da8b9a9
fermino135 <132684644+fermino135@users.noreply.github.com>
2024-03-21 07:48:27 -0300

    Update Dados.py

--- Dados.py
+++ Dados.py
@@ -2,7 +2,7 @@ def cadastrar_dados():
     nome= input("Digite o nome: ")
     endereço = input("Digite o endereço")
     with open("dados.txt", "a")as arquivos:
-        arquivo.write(f"{nome};{endereço}\n")
+        arquivos.write(f"{nome};{endereço}\n")
         print("Dados cadastrados com sucesso!")
         
         def ler_dados():
@@ -12,19 +12,34 @@ def cadastrar_dados():
                     for linha in arquivo:
                         dados = linha.strip().split(';')
                         if len(dados) == 2:
-                            nome, endereço = dadosprint(f"Nome: {nome}, Endereço: {endereço}")
+                            nome, endereço = dados
+                            print(f"Nome: {nome}, Endereço: {endereço}")
                         else:
                             print("Erro ao ler linha do arquivo", linha)
             except FileNotFoundError:
                 print("Nenhum cadastro ainda")
                 
                 def main():
-                    while true:
+                    while True:
                         print("\n==== Menu de opções ====")
                         print("1. Cadastrar dados")
                         print("Ler dados cadastrados")
                         print("3. Sair")
                         
                         opção = input("Escolha uma opção")
+                        
+                        if opção == "1":
+                            cadastrar_dados()
+                        elif opção == "2":
+                            ler_dados()
+                        elif opção == "1":
+                            print("Saindo...")
+                            break
+                        else :
+                            print("Opções inválida. Por favor, escolha uma opção válida")
+                        
+                        if __name__ == "__main__":
+                            main()
+                        
                     
                         
